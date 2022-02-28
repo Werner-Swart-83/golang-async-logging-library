d533b7dbe5de175b1d25324024a1c87bc6a2754e
Werner Swart <werner.swart@willistowerswatson.com>
2022-02-28 16:32:34 +0000

    Urg

--- alog.go
+++ alog.go
@@ -29,21 +29,32 @@ func New(w io.Writer) *Alog {
 		w = os.Stdout
 	}
 	return &Alog{
-		dest:    w,
-		msgCh:   make(chan string),
-		errorCh: make(chan error),
-		m:       &sync.Mutex{},
+		dest:               w,
+		msgCh:              make(chan string),
+		errorCh:            make(chan error),
+		m:                  &sync.Mutex{},
+		shutdownCh:         make(chan struct{}),
+		shutdownCompleteCh: make(chan struct{}),
 	}
 }
 
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
-	for msg := range al.msgCh {
-		go func(msg string) {
-			al.write(msg, nil)
-		}(msg)
+	wg := &sync.WaitGroup{}
+	for {
+		select {
+		case msg := <-al.msgCh:
+			wg.Add(1)
+			go al.write(msg, wg)
+			wg.Wait()
+		case <-al.shutdownCh:
+			al.shutdown()
+			goto End
+		}
 	}
+End:
+	return
 }
 
 func (al Alog) formatMessage(msg string) string {
@@ -54,12 +65,13 @@ func (al Alog) formatMessage(msg string) string {
 }
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
-	al.m.Lock()
 	defer func(m *sync.Mutex) {
 		if r := recover(); r != nil {
 			m.Unlock()
 		}
 	}(al.m)
+
+	al.m.Lock()
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
 	al.m.Unlock()
 
@@ -68,9 +80,13 @@ func (al Alog) write(msg string, wg *sync.WaitGroup) {
 			errorCh <- err
 		}(al.errorCh)
 	}
+
+	wg.Done()
 }
 
 func (al Alog) shutdown() {
+	close(al.msgCh)
+	al.shutdownCompleteCh <- struct{}{}
 }
 
 // MessageChannel returns a channel that accepts messages that should be written to the log.
@@ -88,6 +104,18 @@ func (al Alog) ErrorChannel() <-chan error {
 // Stop shuts down the logger. It will wait for all pending messages to be written and then return.
 // The logger will no longer function after this method has been called.
 func (al Alog) Stop() {
+	al.shutdownCh <- struct{}{}
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
+	go func(shutdownCompleteCh <-chan struct{}, wg *sync.WaitGroup) {
+		for range shutdownCompleteCh {
+			break
+		}
+
+		wg.Done()
+	}(al.shutdownCompleteCh, wg)
+	wg.Wait()
 }
 
 // Write synchronously sends the message to the log output
